6100dab401221e38e027a8e4b72abaacbab89fa1
Sarirhff <19102019@ittelkom-pwt.ac.id>
2022-06-01 02:32:36 +0700

    make signin

--- MountainTravelAndPlanner/app/src/main/java/com/example/mountaintravelandplanner/main/SplashActivity.kt
+++ MountainTravelAndPlanner/app/src/main/java/com/example/mountaintravelandplanner/main/SplashActivity.kt
@@ -3,19 +3,21 @@ package com.example.mountaintravelandplanner.main
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import com.example.mountaintravelandplanner.R
+import com.example.mountaintravelandplanner.databinding.ActivitySplashBinding
+import com.example.mountaintravelandplanner.login.SignInActivity
 
 class SplashActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
 
-        val timeOut = 4000
-        val data = Intent(this@SplashActivity, MainActivity::class.java)
-        Handler().postDelayed({
-            startActivity(data)
-            finish()
-        }, timeOut.toLong())
+    lateinit var binding : ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        binding.buttonSignin.setOnClickListener {
+            val intent = Intent(this, SignInActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
